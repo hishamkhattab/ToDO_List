5e4463dfb3ffb2dc6d8eb186ebd0cd9645f4b0e3
Hisham Khattab <42070333+hishamkhattab@users.noreply.github.com>
2022-01-12 19:53:28 +0200

    add the main style and components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,55 @@
+import { useState} from 'react';
+import { FaEdit, FaTrash } from 'react-icons/fa';
 
 function App() {
+
+  const [item, setItem] = useState('');
+  const [error, setError] = useState({ show: true, msg: 'this is an error', clr: '' });
+
+
   return (
-    <div className="App">
-    </div>
+    <main className="container">
+      <div className="todo-form">
+        {error.show &&
+          <h1 className='alert danger'>{error.msg}</h1>}
+        <form
+          className='d-flex'
+          onSubmit={(e) => e.preventDefault()}
+        >
+          <input
+            type="text"
+            value={item}
+            onChange={(e) => setItem(e.target.value)}
+          />
+          <button className="btn">
+            add
+          </button>
+        </form>
+        <div className="todo-list">
+          <div className="todo-item d-flex">
+            <p>list item #1</p>
+            <div className="todo-control d-flex">
+            <button
+                type='button'
+                className='edit-btn'
+            >
+            <FaEdit />
+            </button>
+            <button
+                type='button'
+                className='delete-btn'
+            >
+            <FaTrash />
+            </button>
+          </div>
+          </div>
+        </div>
+        <button
+          className='clear-btn'
+        >Clear All</button>
+        </div>
+    </main>
   );
-}
+};
 
 export default App;
